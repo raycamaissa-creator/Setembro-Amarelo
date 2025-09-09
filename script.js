870a4df28db853d311ba451a7289665d39b9ba58
raycamaissa-creator <raycamaissa@gmail.com>
2025-09-09 13:31:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const mensagens = [
+  "Você é mais forte do que imagina 💛",
+  "Nunca se esqueça: sua vida importa 🌻",
+  "Você não está sozinho, estamos juntos  🤝",
+  "Acredite: dias melhores virão  ☀️",
+  "Sempre existe esperança 🌈"
+];
 
+document.get.ElementById("btnMensagem").addEventListener("click", () => {
+  const aleatoria = mensagens[Math.random() * mensagens.length)];
+  document.getElementById("mensagem").innerText = aleatoria;
+});
